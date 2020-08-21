c300503bdfe33267dfa9efa0693658b14fcfcc31
raehan <raehan809@elixirpay.com>
2020-08-21 19:55:19 +0900

    Modify

--- todo-app-2nd/src/App.js
+++ todo-app-2nd/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TodoTemplate from './Components/TodoTemplate'
 import TodoInsert from './Components/TodoInsert'
 import TodoList from './Components/TodoList'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'One',
+      checked: true
+    },
+    {
+      id: 2,
+      text: 'Tow',
+      checked: false
+    },
+    {
+      id: 3,
+      text: 'Three',
+      checked: true
+    }
+  ])
+
   return (
     <div className="App">
       <TodoTemplate>
         <TodoInsert />
-        <TodoList />
+        <TodoList todos={todos}/>
       </TodoTemplate>
     </div>
   );
--- todo-app-2nd/src/Components/TodoListItem.js
+++ todo-app-2nd/src/Components/TodoListItem.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md'
 import './TodoListItem.scss'
 
-const TodoListItem = () => {
+const TodoListItem = ({todo}) => {
+  const { text, checked } = todo;
+  
   return (
     <div className="TodoListItem">
       <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
+        { checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
