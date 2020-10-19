13200fbc0876f282f9c318d4d13ed9a6602c6abe
raehan <raehan900809@gmail.com>
2020-10-19 22:23:29 +0900

    Create react router

---
+++ router-tutorial/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>홈</h1>
+      <h2>가장 먼저 보옂는 페이지</h2>
+    </div>
+  );
+};
+
+export default Home;
---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const data = {
+  raehan: {
+    name: '정래한',
+    description: '개발을 좋아하는 개발자'
+  }
+}
+
+const Profile = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Profile;
